4d2003d6e671439a0dffe0f2b2fc528d751c0253
黄楚强 <chuqianghuang@gmail.com>
2013-05-31 00:13:54 +0800

    毕业设计版本

--- speech_recognize/SREngine.h
+++ speech_recognize/SREngine.h
@@ -10,22 +10,22 @@
 class KSREngine  
 {
 public:
+	KSREngine();
+	~KSREngine();
+
+
+	void  SetRuleState(const  WCHAR* pszRuleName, 
+			const WCHAR*pszValue, BOOL fActivate);
+
+	bool  LoadCmdFromFile(QString XMLFileName);
+	void  InitializeSapi(HWND hWnd, UINT Msg);
+
+public:
 	CComPtr<ISpRecognizer> m_pRecognizer;
 	CComPtr<ISpRecoContext> m_pRecoCtxt;
 	CComPtr<ISpRecoGrammar> m_pCmdGram;
 	CComPtr<ISpAudio> m_pAudio;
 	CComPtr<ISpVoice> m_pVoice;
-
-public:
-	KSREngine();
-	~KSREngine();
-
-public:
-	void  SetRuleState(const  WCHAR* pszRuleName, const WCHAR*pszValue, BOOL fActivate);
-	bool  LoadCmdFromFile(QString XMLFileName);
-    void  InitializeSapi(HWND hWnd, UINT Msg);
-
-
 };
 
 #endif // SRENGINE_H
